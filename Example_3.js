a19e8326ccfd6124968c630961892b1810d75ada
fgdota2 <68871109+fgdota2@users.noreply.github.com>
2020-12-02 06:12:06 +0300

    123

--- Example_3.js
+++ Example_3.js
@@ -1,12 +1,12 @@
 function changeArray(arr) {
             if (arr.length % 2 == 0) {
-                let removed = arr.splice(0, arr.length/2)
-                let final = arr.concat(removed)
+                 removed = arr.splice(0, arr.length/2)
+                 final = arr.concat(removed)
                 console.log(final)}
             else {
-                let removed = (arr.splice(0, (arr.length/2)+1))
-                let final = arr.concat(removed,removed.splice(0, arr.length))
+                 removed = (arr.splice(0, (arr.length/2)+1))
+                 final = arr.concat(removed,removed.splice(0, arr.length))
                 console.log(final)
         }
 }
-changeArray([ 1, 2, 3, 4, 5, 6, 7, 8])
+changeArray([ 1, 2, 3, 4, 5, 6, 7])
